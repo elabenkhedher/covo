21c0ada84f21bafc0a7234a6679a51afa82bed1f
elabenkhedher <elabenkedher@gmail.com>
2026-04-25 22:07:59 +0100

    temp

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -29,4 +29,10 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+    #[Route(path: '/forgot-password', name: 'app_forgot_password')]
+    public function forgotPassword(): Response
+    {
+        return $this->render('forgot-password.html.twig');
+    }
 }
