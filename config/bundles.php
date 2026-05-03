5aaa8fc42fe2b51955657ba3afd2e5b0d6b1f4bb
Obaydz <oubeidallahzmander@gmail.com>
2026-05-03 15:39:11 +0100

    Added Mercure Bundle for WebSockets (GPS)

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
 ];
